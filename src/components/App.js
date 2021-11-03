8c2467ee1b43ad0e34a536105167e0b57d448e88
Tayebe kavousi <tayebekavousi68@gmail.com>
2021-11-03 09:39:50 +0800

    App.js changed to make a navbar-part 1

--- src/components/App.js
+++ src/components/App.js
@@ -13,37 +13,44 @@ export default class App extends React.Component {
     return (
       <Router>
         <div className="container-fluid">
-          <h1>White Board</h1>
-          <nav className="nav navbar">
-            <li>
-              <Link to="/TopicPills.js">Topics</Link>
-            </li>
-            <li>
-              <Link to="/LessonTabs.js">Lessons</Link>
-            </li>
-            <li>
-              <Link to="/ModuleList.js">Modules</Link>
-            </li>
-            <li>
-              <Link to="/Course.js">Courses</Link>
-            </li>
-            <li>
-              <Link to="/Login.js">Login</Link>
-            </li>
-            <li>
-              <Link to="/Register.js">Register</Link>
-            </li>
-          </nav>
-          <Switch>
-            <Route path="/TopicPills.js"><TopicPills/></Route>                          
-            <Route path="/LessonTabs.js"><LessonTabs/></Route>            
-            <Route path="/ModuleList.js" ><ModuleList/></Route>             
-            <Route path="/Course.js"><Course/></Route>
-            <Route path="/course/:courseId"><CourseEditor/></Route>
-            <Route path="/Login.js"><Login/></Route>
-            <Route path="/Register.js"><Register/></Route>
-            <Redirect to='/App'/>
-          </Switch>
+          <nav className="navbar navbar-expand-lg sticky-top navbar-light bg-light nav-fill">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <a className="navbar-brand" href="#">White Board</a>
+                <ul className="navbar-nav nav-pills ">
+                    <li className="nav-item active">
+                      <Link to="/TopicPills.js">Topics</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/LessonTabs.js">Lessons</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/ModuleList.js">Modules</Link>
+                    </li> 
+                    <li className="nav-item">
+                      <Link to="/Course.js">Courses</Link>
+                    </li> 
+                    <li className="nav-item">
+                      <Link to="/Login.js">Login</Link>
+                    </li>                 
+                    <li className="nav-item">
+                      <Link to="/Register.js">Register</Link>
+                    </li>                 
+                </ul>
+            </div>
+            </nav>
+            <Switch>
+              <Route path="/TopicPills.js"><TopicPills/></Route>                          
+              <Route path="/LessonTabs.js"><LessonTabs/></Route>            
+              <Route path="/ModuleList.js" ><ModuleList/></Route>             
+              <Route path="/Course.js"><Course/></Route>
+              <Route path="/course/:courseId"><CourseEditor/></Route>
+              <Route path="/Login.js"><Login/></Route>
+              <Route path="/Register.js"><Register/></Route>
+              <Redirect to='/App'/>
+            </Switch>
         </div>
       </Router>
     );
