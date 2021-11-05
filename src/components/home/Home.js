32ea860dfa2e5581849d728a42e5a4bd3be88a07
Tayebe kavousi <tayebekavousi68@gmail.com>
2021-11-05 12:08:43 +0800

    showing courses in carousel

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -12,9 +12,34 @@ export default class Home extends Component {
         this.courseService.findAllCourses()
         .then(courses => {
             this.setState({courses: courses});
-        });
+        }).then(()=>console.log(this.state.courses));
     }
+
     render(){
+
+        const imgIndicators =
+            <ol className="carousel-indicators">
+            { this.state.courses.map((course, index) =>{
+            return(
+                <li data-target="#courseCarousel" data-slide-to={`${index}`} className={index=== 0?"active":""} key={index}></li>
+                )})
+            }
+            </ol>
+        const images = 
+            <div className="carousel-inner">
+            {this.state.courses.map((course, index) =>{
+            return(
+                    <div className={index=== 0?"carousel-item active":"carousel-item"} key={index}>
+                        <img className="d-block w-100" src="https://picsum.photos/id/1002/600/300" alt={index}/>
+                        <div className="carousel-caption d-none d-md-block container">
+                            <h1>{course.title}</h1>
+                            <h3>Created at : {course.created}</h3>
+                        </div>
+                    </div>
+            )})
+            }
+            </div>
+        
         return (
             <div className="container centered-card">
                 <h1>White Board</h1>
@@ -22,34 +47,8 @@ export default class Home extends Component {
                 <h3>Build skills with courses, certificates, and degrees online from world-class universities and companies.</h3>
                 
                 <div id="courseCarousel" className="carousel slide carousel-fade" data-ride="carousel" data-interval="5000" data-pause="hover">
-                    <ol className="carousel-indicators">
-                        <li data-target="#courseCarousel" data-slide-to="0" className="active"></li>
-                        <li data-target="#courseCarousel" data-slide-to="1"></li>
-                        <li data-target="#courseCarousel" data-slide-to="2"></li>
-                    </ol>
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img className="d-block w-100" src="https://picsum.photos/id/0/600/300?grayscale" alt="First slide"/>
-                            <div className="carousel-caption d-none d-md-block container" style={{color:'rgb(1, 62, 73)'}} >
-                                <h1>Course 1</h1>
-                                <h3>a new  course to teach you Bootstrap</h3>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img className="d-block w-100" src="https://picsum.photos/id/119/600/300?grayscale" alt="Second slide"/>
-                            <div className="carousel-caption d-none d-md-block container" style={{color:'rgb(1, 62, 73)'}}>
-                                <h1>Course 2</h1>
-                                <p>a new  course to teach you Javascript</p>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img className="d-block w-100" src="https://picsum.photos/id/180/600/300?grayscale" alt="Third slide"/>
-                            <div className="carousel-caption d-none d-md-block container" style={{color:'rgb(1, 62, 73)'}}>
-                                <h1>Course 3</h1>
-                                <p>a new  course to teach you React</p>
-                            </div>
-                        </div>
-                    </div>
+                    {imgIndicators}
+                    {images}
                     <a className="carousel-control-prev" href="#courseCarousel" role="button" data-slide="prev">
                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span className="sr-only">Previous</span>
