f1e0d48fe8851a94e89902172f7680de7de8d3dc
Tayebe kavousi <tayebekavousi68@gmail.com>
2021-11-05 08:49:34 +0800

    making carousel in home page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import "../node_modules/font-awesome/css/font-awesome.css";
 
 ReactDOM.render(
