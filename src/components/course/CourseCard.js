552b079c45e1fca442c6f8282b55a4746273fd9f
Tayebe kavousi <tayebekavousi68@gmail.com>
2021-11-14 17:05:11 +0800

    editing home component

--- src/components/course/CourseCard.js
+++ src/components/course/CourseCard.js
@@ -1,38 +1,26 @@
 import React from 'react';
 
-function RenderCard({course, deleteCourse}){
-  return(
-    <div className="col-sm-3">
-      <div className="card">
-        <img className="card-img-top" src={course.imgURL} alt="decorative"/>
-        <div className="card-body">
-          <h5 className="card-title">{course.title}</h5>
-          <p className="card-text">
-            This is a course.
-          </p>
-        </div>
-        <div className="card-footer">
-        <p className="card-text">
-            <small className="text-muted">Owner:{course.owner}</small>
-          </p>
-          <p className="card-text">
-            <small className="text-muted">created at {course.created}</small>
-          </p>
-          <p className="card-text">
-            <small className="text-muted">Last modified at {course.modified}</small>
-          </p>
-          <button className="btn btn-primary" onClick={()=>deleteCourse(course.id)}>delete</button>
-        </div>
-      </div>
-    </div> 
-  );
-}
 
-export default function CourseCard({courses, deleteCourse}){  
+export default function CourseCard({courses}){  
   return (
     <div className="row">
-      {courses.map( (course,i) => <RenderCard key={i} course={course} deleteCourse={deleteCourse}/>)}
-    </div>
+      {courses.map((course, index) => {return(
+        <div className="col-sm-3" key={index} style={{width:"33%"}}>
+          <div className="card">
+            <img className="card-img-top" src={`${course.imgURL}/1500`} alt={course.title}/>
+            <div className="card-body">
+              <h5 className="card-title">{course.title}</h5>
+              <p className="card-text">
+                <small className="text-muted">{course.description}</small>
+              </p>
+              <p className="card-text">
+                <small className="text-muted">Created By :{course.owner}</small>
+              </p>
+            </div>
+          </div>
+        </div> 
+      )})}   
+    </div> 
   );
 }
 
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CourseService from '../../services/CourseService'; 
+import CourseCard from "../course/CourseCard";
 
 
 export default class Home extends Component {
@@ -16,48 +17,14 @@ export default class Home extends Component {
     }
 
     render(){
-
-        const imgIndicators =
-            <ol className="carousel-indicators">
-            { this.state.courses.map((course, index) =>{
-            return(
-                <li data-target="#courseCarousel" data-slide-to={`${index}`} className={index=== 0?"active":""} key={index}></li>
-                )})
-            }
-            </ol>
-        const images = 
-            <div className="carousel-inner">
-            {this.state.courses.map((course, index) =>{
-            return(
-                    <div className={index=== 0?"carousel-item active":"carousel-item"} key={index}>
-                        <img className="d-block w-100" src="https://picsum.photos/id/1002/600/300" alt={index}/>
-                        <div className="carousel-caption d-none d-md-block container">
-                            <h1>{course.title}</h1>
-                            <h3>Created at : {course.created}</h3>
-                        </div>
-                    </div>
-            )})
-            }
-            </div>
-        
         return (
             <div className="container centered-card">
                 <h1>White Board</h1>
                 <h2>Learn Without Limits</h2>
                 <h3>Build skills with courses, certificates, and degrees online from world-class universities and companies.</h3>
                 
-                <div id="courseCarousel" className="carousel slide carousel-fade" data-ride="carousel" data-interval="5000" data-pause="hover">
-                    {imgIndicators}
-                    {images}
-                    <a className="carousel-control-prev" href="#courseCarousel" role="button" data-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="sr-only">Previous</span>
-                    </a>
-                    <a className="carousel-control-next" href="#courseCarousel" role="button" data-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="sr-only">Next</span>
-                    </a>
-                </div>
+                    <CourseCard courses={this.state.courses}/>
+                
             </div>
         ) 
     }
